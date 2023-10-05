859366cf347a980877b98de8f88654d3eb7ca471
thiagomoraiis <thiagomorais2605@gmail.com>
2023-10-05 17:33:32 -0300

    inserindo mixin de login required

--- reservas/views.py
+++ reservas/views.py
@@ -1,5 +1,3 @@
-from typing import Any
-from django.db.models.query import QuerySet
 from django.views.generic import (
     ListView,
     CreateView,
@@ -12,41 +10,43 @@ from django.contrib.auth.decorators import login_required
 from .models import Reserva
 from .forms import ReservaModelForm
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # @login_required(login_url='/user/login')
-class IndexListView(ListView):
+class IndexListView(LoginRequiredMixin, ListView):
     model = Reserva
     template_name = 'reservas/pages/index.html'
     queryset = Reserva.objects.all()
     context_object_name = 'reservas'
+    login_url = '/user/login/'
 
-# @login_required(login_url='/user/login')
-class ReservaCreateView(CreateView):
+class ReservaCreateView(LoginRequiredMixin, CreateView):
     model = Reserva
     form_class = ReservaModelForm
     template_name = 'reservas/pages/reserva_form.html'
     success_url = reverse_lazy('index')
+    login_url = '/user/login/'
 
-# @login_required(login_url='/user/login')
-class ReservaDetailView(DetailView):
+class ReservaDetailView(LoginRequiredMixin, DetailView):
     model = Reserva
     queryset = Reserva.objects.all()
     context_object_name = 'reserva'
     pk_url_kwarg = 'id'
     template_name = 'reservas/pages/reserva.html'
+    login_url = '/user/login/'
 
-# @login_required(login_url='/user/login')
-class ReservaUpdateView(UpdateView):
+class ReservaUpdateView(LoginRequiredMixin, UpdateView):
     model = Reserva
     form_class = ReservaModelForm
     pk_url_kwarg = 'id'
     template_name = 'reservas/pages/reserva_form.html'
+    login_url = '/user/login/'
     success_url = reverse_lazy('index')
 
-# @login_required(login_url='/user/login')
-class ReservaDeleteView(DeleteView):
+class ReservaDeleteView(LoginRequiredMixin, DeleteView):
     model = Reserva
     context_object_name = 'reserva'
     template_name = 'reservas/pages/confirmacao_exclusao.html'
     pk_url_kwarg = 'id'
     success_url = reverse_lazy('index')
+    login_url = '/user/login/'
--- user/forms.py
+++ user/forms.py
@@ -3,14 +3,22 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    username = forms.CharField(label='Username', widget=forms.TextInput(
+        attrs={'class':'form-control'})
+    )
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'})
+    )
 
 class UserForm(UserCreationForm):
 
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
-
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'})
+        }
 
 
